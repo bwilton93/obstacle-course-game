56171c14527fe3416bf4dd7d15288dd0febb3068
Barney W <B.Wilton1993@gmail.com>
2021-08-29 23:24:42 +0100

    Created scoring and lives mechanic

---
+++ Obstacle Course Game/Assets/Scripts/Scoring.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+    [SerializeField] public int playerLives = 3;
+    [SerializeField] public int playerScore = 0;
+}
