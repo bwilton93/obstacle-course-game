3cdc8093a03d66ce3865ac08e92aeed581982646
Barney W <B.Wilton1993@gmail.com>
2021-08-29 19:34:15 +0100

    Reformatted some code

--- Obstacle Course Game/Assets/Scripts/PlayerController.cs
+++ Obstacle Course Game/Assets/Scripts/PlayerController.cs
@@ -9,13 +9,26 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Get the horizontal and vertical axis, default bound to arrow keys or WASDs.
+        movePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move the player with the arrow keys or WASD");
+        Debug.Log("Try to avoid the walls while you navigate the course!");
+        Debug.Log("How about opening the door first?");    
+    }
+
+    void movePlayer()
+    {
+        // Get the horizontal and vertical axis, default bound to arrow keys or WASD.
         // Range -1 to 1
         float xTranslation = Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
         float zTranslation = Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
